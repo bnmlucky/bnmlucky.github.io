3c9cffd9862f4372eda7abb42709c246cc1d6200
Natalia Titova <bnmlucky@gmail.com>
2019-10-16 15:20:57 -0400

    built out the basic layout and added some styling

--- funnyFortunes/app.js
+++ funnyFortunes/app.js
@@ -1,12 +1,29 @@
 $(() => {
-  const newGif = "money";
-  const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=OBitdmb0SaKAnBE6xlHlIFrUHJW6YfFU&q=${newGif}&limit=1&offset=0&rating=PG-13&lang=en`;
+  const newGif = "coffee";
+  const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=OBitdmb0SaKAnBE6xlHlIFrUHJW6YfFU&q=${newGif}&limit=10&offset=25&rating=G&lang=en`;
   console.log(endpoint);
 
   const handleData = data => {
-    console.log(data.data[0].url);
-    const image = $("<img>").attr("src", data.data[0].url);
-    $("body").append(image);
+    const image = $("<img>").attr("src", data.data[0].images.downsized.url);
+    $(".container").append(image);
+    const image1 = $("<img>").attr("src", data.data[1].images.downsized.url);
+    $(".container").append(image1);
+    const image2 = $("<img>").attr("src", data.data[2].images.downsized.url);
+    $(".container").append(image2);
+    const image3 = $("<img>").attr("src", data.data[3].images.downsized.url);
+    $(".container").append(image3);
+    const image4 = $("<img>").attr("src", data.data[4].images.downsized.url);
+    $(".container").append(image4);
+    const image5 = $("<img>").attr("src", data.data[5].images.downsized.url);
+    $(".container").append(image5);
+    const image6 = $("<img>").attr("src", data.data[6].images.downsized.url);
+    $(".container").append(image6);
+    const image7 = $("<img>").attr("src", data.data[7].images.downsized.url);
+    $(".container").append(image7);
+    const image8 = $("<img>").attr("src", data.data[8].images.downsized.url);
+    $(".container").append(image8);
+    const image9 = $("<img>").attr("src", data.data[9].images.downsized.url);
+    $(".container").append(image9);
   };
   $.ajax({
     url: endpoint
@@ -26,3 +43,14 @@ const handleData = (data) => {
 }
 
 $.ajax({ url: endpoint }).then(handleData) */
+
+// const $gifDiv = $("<div>");
+// $gifDiv.attr('id', ``)
+// $gifDiv.on("click", event => {
+//   const $clickedTarget = $(event.currentTarget);
+//   console.log("You clicked: ", $clickedTarget);
+//   if ($clickedTarget.attr('id') === )
+// });
+
+// const randomIndex = Math.floor(Math.random() * arr.length);
+// arr[randomIndex]
