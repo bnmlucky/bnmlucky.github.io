95b5aed52135ef194b6dd8076862202511c64369
Natalia Titova <bnmlucky@gmail.com>
2019-10-18 16:12:57 -0400

    I have predictions assigned to each tile - hardcoded, they appear on click, the giphy tile grid hides on selection

--- funnyFortunes/app.js
+++ funnyFortunes/app.js
@@ -14,70 +14,93 @@ $(() => {
     $(".container").append(askUser);
 
     const handleData = data => {
-      const image0 = $("<img>")
-        .attr("src", data.data[0].images.downsized.url)
-        .addClass("square");
-      $(".container").append(image0);
-      const image1 = $("<img>")
-        .attr("src", data.data[1].images.downsized.url)
-        .addClass("square");
-      $(".container").append(image1);
-      const image2 = $("<img>")
-        .attr("src", data.data[2].images.downsized.url)
-        .addClass("square");
-      $(".container").append(image2);
-      const image3 = $("<img>")
-        .attr("src", data.data[3].images.downsized.url)
-        .addClass("square");
-      $(".container").append(image3);
-      const image4 = $("<img>")
-        .attr("src", data.data[4].images.downsized.url)
-        .addClass("square");
-      $(".container").append(image4);
-      const image5 = $("<img>")
-        .attr("src", data.data[5].images.downsized.url)
-        .addClass("square");
-      $(".container").append(image5);
-      const image6 = $("<img>")
-        .attr("src", data.data[6].images.downsized.url)
-        .addClass("square");
-      $(".container").append(image6);
-      const image7 = $("<img>")
-        .attr("src", data.data[7].images.downsized.url)
-        .addClass("square");
-      $(".container").append(image7);
-      const image8 = $("<img>")
-        .attr("src", data.data[8].images.downsized.url)
-        .addClass("square");
-      $(".container").append(image8);
+      for (let i = 0; i < 9; i++) {
+        const image = $("<img>")
+          .attr("src", data.data[i].images.downsized.url)
+          .addClass("square")
+          .addClass(assignRandomClass());
+        $(".container").append(image);
+        givePrediction(image);
+        // image.on("click", () => {
+        //   console.log(`clicked ${i}`);
+        //   // if/else statement to check if it has super
+      }
     };
     $.ajax({
       url: endpoint
     }).then(handleData);
   });
+
+  const givePrediction = clickedImg => {
+    clickedImg.on("click", event => {
+      if ($(clickedImg).hasClass("good")) {
+        $(".container")
+          .hide("slow")
+          .delay(4000);
+        const $predictionDiv = $(".prediction");
+        const $thisPrediction = $("<p>");
+        $thisPrediction
+          .text("Someone is looking up to you.  Don't let that person down.")
+          .addClass("prediction-text");
+        $thisPrediction.appendTo($predictionDiv);
+        // $predictionDiv.show();
+        console.log(
+          "Someone is looking up to you.  Don't let that person down."
+        );
+      } else if ($(clickedImg).hasClass("awesome")) {
+        console.log(
+          "Run in the direction of your dreams.  They are closer than you think!"
+        );
+      } else if ($(clickedImg).hasClass("OK")) {
+        console.log(
+          "Set your worries aside for a while and go to a party. The right path will emerge when you are more relaxed."
+        );
+      } else if ($(clickedImg).hasClass("alright")) {
+        console.log(
+          "You can't get what you want by sitting all by yourself in a corner.  Get out into the world, go for it, and you will get it!"
+        );
+      } else if ($(clickedImg).hasClass("wow")) {
+        console.log(
+          "What you want to achieve lies outside of your comfort zone.  Gear up and go get it!"
+        );
+      } else if ($(clickedImg).hasClass("whatever")) {
+        console.log(
+          "Life has a great surprise in store for you, get ready.  Don't do anything stupid while you wait."
+        );
+      } else if ($(clickedImg).hasClass("cool")) {
+        console.log(
+          "You'll survive whatever you are going through, write a survival guide and get on New York Times Bestseller list with it"
+        );
+      } else if ($(clickedImg).hasClass("great")) {
+        console.log(
+          "There is logic in seemingly random events that happen in your life.  Trust the proces and it'll take you where you want to be.  Follow the white rabbit."
+        );
+      } else if ($(clickedImg).hasClass("super")) {
+        console.log(
+          "There is no easy path to where you want to be, but you'll make a lot of friends along the way"
+        );
+      } else {
+        console.log("something");
+      }
+    });
+  };
 });
+//$(".container").html("");
 
-// /* $(()=> {
-// const handleData = (data) => {
-//   console.log(data.Title)
-
-//   const $title = $('<h1>').text(data.Title) // create h1 and put the title inside
-//   $('.container').append($title) // add the title to the container
-
-//   const $director = $('<h2>').text(`Directed by the one and only ${data.Director}`) // create the h2 and put the director inside
-//   $('.container').append($director) // add the director to the container
-
-// }
-
-// $.ajax({ url: endpoint }).then(handleData) */
-
-// // const $gifDiv = $("<div>");
-// // $gifDiv.attr('id', ``)
-// // $gifDiv.on("click", event => {
-// //   const $clickedTarget = $(event.currentTarget);
-// //   console.log("You clicked: ", $clickedTarget);
-// //   if ($clickedTarget.attr('id') === )
-// // });
-
-// // const randomIndex = Math.floor(Math.random() * arr.length);
-// // arr[randomIndex]
+const randomClassArray = [
+  "good",
+  "awesome",
+  "OK",
+  "alright",
+  "wow",
+  "whatever",
+  "cool",
+  "great",
+  "super"
+];
+const assignRandomClass = () => {
+  const randomClassIndex = Math.floor(Math.random() * randomClassArray.length);
+  const randomClassValue = randomClassArray[randomClassIndex];
+  return randomClassValue;
+};
+assignRandomClass();
