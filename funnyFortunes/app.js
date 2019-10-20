366f780115f2c9e0e47f5d0e581346fbe6e0be12
Natalia Titova <bnmlucky@gmail.com>
2019-10-20 17:46:30 -0400

    Bare bones MVP

--- funnyFortunes/app.js
+++ funnyFortunes/app.js
@@ -45,41 +45,151 @@ $(() => {
           .addClass("prediction-text");
         $thisPrediction.appendTo($predictionDiv);
         $predictionDiv.show().delay(7000);
-        console.log(
-          "Someone is looking up to you.  Don't let that person down."
-        );
+        //
       } else if ($(clickedImg).hasClass("awesome")) {
-        console.log(
-          "Run in the direction of your dreams.  They are closer than you think!"
+        $(".container")
+          .hide("slow")
+          .delay(1000);
+        const $predictionDiv = $(".prediction");
+        const $predictionTitle = $("<h1>").text(
+          "Here's what you need to know:"
         );
+        $predictionTitle.appendTo($predictionDiv);
+        const $thisPrediction = $("<p>");
+        $thisPrediction
+          .text(
+            "Run in the direction of your dreams.  They are closer than you think!"
+          )
+          .addClass("prediction-text");
+        $thisPrediction.appendTo($predictionDiv);
+        $predictionDiv.show().delay(7000);
+        //
       } else if ($(clickedImg).hasClass("OK")) {
-        console.log(
-          "Set your worries aside for a while and go to a party. The right path will emerge when you are more relaxed."
+        $(".container")
+          .hide("slow")
+          .delay(1000);
+        const $predictionDiv = $(".prediction");
+        const $predictionTitle = $("<h1>").text(
+          "Here's what you need to know:"
         );
+        $predictionTitle.appendTo($predictionDiv);
+        const $thisPrediction = $("<p>");
+        $thisPrediction
+          .text(
+            "Set your worries aside for a while and go to a party. The right path will emerge when you are more relaxed."
+          )
+          .addClass("prediction-text");
+        $thisPrediction.appendTo($predictionDiv);
+        $predictionDiv.show().delay(7000);
+        //
       } else if ($(clickedImg).hasClass("alright")) {
-        console.log(
-          "You can't get what you want by sitting all by yourself in a corner.  Get out into the world, go for it, and you will get it!"
+        $(".container")
+          .hide("slow")
+          .delay(1000);
+        const $predictionDiv = $(".prediction");
+        const $predictionTitle = $("<h1>").text(
+          "Here's what you need to know:"
         );
+        $predictionTitle.appendTo($predictionDiv);
+        const $thisPrediction = $("<p>");
+        $thisPrediction
+          .text(
+            "You can't get what you want by sitting all by yourself in a corner.  Get out into the world, go for it, and you will get it!"
+          )
+          .addClass("prediction-text");
+        $thisPrediction.appendTo($predictionDiv);
+        $predictionDiv.show().delay(7000);
+        //
       } else if ($(clickedImg).hasClass("wow")) {
-        console.log(
-          "What you want to achieve lies outside of your comfort zone.  Gear up and go get it!"
+        $(".container")
+          .hide("slow")
+          .delay(1000);
+        const $predictionDiv = $(".prediction");
+        const $predictionTitle = $("<h1>").text(
+          "Here's what you need to know:"
         );
+        $predictionTitle.appendTo($predictionDiv);
+        const $thisPrediction = $("<p>");
+        $thisPrediction
+          .text(
+            "What you want to achieve lies outside of your comfort zone. Gear up and go get it! Take the plunge."
+          )
+          .addClass("prediction-text");
+        $thisPrediction.appendTo($predictionDiv);
+        $predictionDiv.show().delay(7000);
+        //
       } else if ($(clickedImg).hasClass("whatever")) {
-        console.log(
-          "Life has a great surprise in store for you, get ready.  Don't do anything stupid while you wait."
+        $(".container")
+          .hide("slow")
+          .delay(1000);
+        const $predictionDiv = $(".prediction");
+        const $predictionTitle = $("<h1>").text(
+          "Here's what you need to know:"
         );
+        $predictionTitle.appendTo($predictionDiv);
+        const $thisPrediction = $("<p>");
+        $thisPrediction
+          .text(
+            "Life has a great surprise in store for you, get ready.  Don't do anything stupid while you wait."
+          )
+          .addClass("prediction-text");
+        $thisPrediction.appendTo($predictionDiv);
+        $predictionDiv.show().delay(7000);
+        //
       } else if ($(clickedImg).hasClass("cool")) {
-        console.log(
-          "You'll survive whatever you are going through, write a survival guide and get on New York Times Bestseller list with it"
+        $(".container")
+          .hide("slow")
+          .delay(1000);
+        const $predictionDiv = $(".prediction");
+        const $predictionTitle = $("<h1>").text(
+          "Here's what you need to know:"
         );
+        $predictionTitle.appendTo($predictionDiv);
+        const $thisPrediction = $("<p>");
+        $thisPrediction
+          .text(
+            "You'll survive whatever you are going through, write a survival guide and get on New York Times Bestseller list with it"
+          )
+          .addClass("prediction-text");
+        $thisPrediction.appendTo($predictionDiv);
+        $predictionDiv.show().delay(7000);
+        //
       } else if ($(clickedImg).hasClass("great")) {
-        console.log(
-          "There is logic in seemingly random events that happen in your life.  Trust the proces and it'll take you where you want to be.  Follow the white rabbit."
+        $(".container")
+          .hide("slow")
+          .delay(1000);
+        const $predictionDiv = $(".prediction");
+        const $predictionTitle = $("<h1>").text(
+          "Here's what you need to know:"
         );
+        $predictionTitle.appendTo($predictionDiv);
+        const $thisPrediction = $("<p>");
+        $thisPrediction
+          .text(
+            "There is logic in seemingly random events that happen in your life.  Trust the proces and it'll take you where you want to be."
+          )
+          .addClass("prediction-text");
+        $thisPrediction.appendTo($predictionDiv);
+        $predictionDiv.show().delay(7000);
+        //
       } else if ($(clickedImg).hasClass("super")) {
-        console.log(
-          "There is no easy path to where you want to be, but you'll make a lot of friends along the way"
+        $(".container")
+          .hide("slow")
+          .delay(1000);
+        const $predictionDiv = $(".prediction");
+        const $predictionTitle = $("<h1>").text(
+          "Here's what you need to know:"
         );
+        $predictionTitle.appendTo($predictionDiv);
+        const $thisPrediction = $("<p>");
+        $thisPrediction
+          .text(
+            "There is no easy path to where you want to be, but you'll make a lot of friends along the way."
+          )
+          .addClass("prediction-text");
+        $thisPrediction.appendTo($predictionDiv);
+        $predictionDiv.show().delay(7000);
+        //
       } else {
         console.log("something");
       }
